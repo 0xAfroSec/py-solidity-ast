35a9aebecd3f8eec431874f269171da757380b3e
dev0cloo <etor.ocloo@gmail.com>
2023-07-21 17:28:33 +0000

    Improve Class Creation

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from pprint import pprint
-from dataclasses import make_dataclass
+from dataclasses import make_dataclass, dataclass
 
 ast = {
     "absolutePath": "./contracts/CrowdFunding.sol",
@@ -31,22 +31,44 @@ ast = {
     ]
 }
 
-# def create_class()
-# extract keys and values from ast
-key_data = []
-for key, value in ast.items():
-    # get data type for each value
-    key_data.append((key, type(value)))
-pprint(key_data)
 
-# # create a dataclass with each key and the value as the data type
-class_name = f'{ast.get("nodeType")}Node'
-new_class = make_dataclass(class_name, key_data)
+'''
+Given a dictionary as input, Dynamically creates a data class object with the keys as attributes and the values as data type for the attributes. Returns a data class object.
+and
+'''
 
-obj = new_class(**ast)
 
-pprint(obj)
+def create_class(ast_dict):
+    # extract keys and values from dictionary
+    key_data = []
+    for key, value in ast.items():
+        # get data type for each value
+        key_data.append((key, type(value)))
 
-print(obj.nodes)
+    # Create class_name from Node Type
+    class_name = f'{ast.get("nodeType", "no_node_type")}Node'
+    # create a dataclass with each key and the value as the data type
+    new_class = make_dataclass(class_name, key_data)
 
-# unpack dictionary into class
+    # Method to return node type
+    def node_type(self):
+        return f"The Node Type is: {self.nodeType}"
+
+    # add node_type method to new class instance
+    new_class.node_type = node_type
+
+    return new_class
+
+
+@dataclass
+class AstClass():
+    SourceUnit: create_class(ast)
+
+
+# create a new class object
+unit = create_class(ast)
+# create new class instance
+sourceunit = unit(**ast)
+
+
+print(sourceunit.node_type())
